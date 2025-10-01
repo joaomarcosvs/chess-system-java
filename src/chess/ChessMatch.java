1906071e88cc9acf4f32f98aa826a06694c45661
João Marcos Vieira <joaomarcos.vieirasantos@gmail.com>
2025-10-01 14:56:45 -0300

    Implementing possible moves of rock

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -26,19 +26,22 @@ public class ChessMatch {
         return mat;
     }
 
+    public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+        Position source = sourcePosition.toPosition();
+        Position target = targetPosition.toPosition();
+
+        validateSourcePosition(source);
+        validateTargetPosition(source, target);
+
+        Piece capturedPiece =  makeMove(source, target);
+        return (ChessPiece) capturedPiece;
+    }
+
     private Piece makeMove(Position source, Position target) {
         Piece p = board.removePiece(source);
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
         return capturedPiece;
-    }
-
-    public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
-        Position source = sourcePosition.toPosition();
-        Position target = targetPosition.toPosition();
-        validateSourcePosition(source);
-        Piece capturedPiece =  makeMove(source, target);
-        return (ChessPiece) capturedPiece;
     }
 
     private void validateSourcePosition(Position position) {
@@ -47,6 +50,12 @@ public class ChessMatch {
         }
         if (!board.piece(position).isThereAnyPossibleMove()) {
             throw new ChessException("There is no possible moves for the chosen piece");
+        }
+    }
+
+    private void validateTargetPosition(Position source, Position target) {
+        if (!board.piece(source).possibleMove(target)) {
+            throw new ChessException("The chosen piece can't move to target position");
         }
     }
 
